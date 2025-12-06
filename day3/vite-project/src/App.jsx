dd5bff221168fa9de6d33a2cb582f09a5248622b
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-12-07 00:00:03 +0200

    day 3

---
+++ day3/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>Hello Dojo !</h1>
+        <h2>Things i need to do</h2>
+        <ul>
+            <li>Learn React</li>
+            <li>Learn Node</li>
+            <li>Learn MongoDB</li>
+            <li>Learn Express</li>
+        </ul>
+
+    </>
+  )
+}
+
+export default App
