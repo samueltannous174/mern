286ccdbd9f7263fabed1a28b6d3d9acc16df1f1f
samueltannous <106975094+samueltannous174@users.noreply.github.com>
2025-12-18 16:42:15 +0200

    day11

--- day11/client/src/components/Form.jsx
+++ day11/client/src/components/Form.jsx
@@ -3,20 +3,31 @@ import React, { useEffect, useState } from "react";
 
 const Form = (props)=>{
     const [name, setName] = useState("");
-      useEffect(() => {
+    const [frontErrors, setFrontErrors] = useState({});
+
+    useEffect(() => {
     if (props.current?.name) {
-      setName(props.current.name);
+    setName(props.current.name);
     }
-  }, [props.current]);
+}, [props.current]);
+
+    
 
     console.log("Current prop:", props.current);
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setFrontErrors({});
         props.callBack({ name });
     };
+
     const handleNameChange = (e) => {
         setName(e.target.value);
+        if (e.target.value.length < 3) {
+            setFrontErrors({ name: "Name must be at least 3 characters long." });
+        }else{
+            setFrontErrors({});
+        }
     }
 
     return (
@@ -29,6 +40,12 @@ const Form = (props)=>{
             <input onChange={(e)=> handleNameChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Enter name" 
             value={name}
             />
+            {frontErrors?.name && ( 
+                <p className="text-red-500 text-sm">
+                    {frontErrors.name}
+                </p>
+            )}
+
             </div>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >
                 {props.buttonText}
